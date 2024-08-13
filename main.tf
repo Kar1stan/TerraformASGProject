5276387bbb3f3afc1ae9b89f0cda7b3e0859cb9f
Karastan Vladislav <90850632+Kar1stan@users.noreply.github.com>
2024-08-13 22:35:12 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,18 @@ provider "random" {}
 
 resource "random_pet" "name" {}
 
+resource "aws_db_instance" "example" {
+  identifier_prefix   = "terraform-up-and-running"
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t2.micro"
+  skip_final_snapshot = true
+  db_name             = "example_database"
+  # How should we set the username and password?
+  username = "???"
+  password = "???"
+}
+
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-0fb653ca2d3203ac1"
   instance_type   = "t2.micro"
