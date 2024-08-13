0b9bea76576ec58f2424547a82489d1e494a0121
Karastan Vladislav <90850632+Kar1stan@users.noreply.github.com>
2024-08-13 22:45:16 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "alb_dns_name" {
   value       = aws_lb.example.dns_name
   description = "The domain name of the load balancer"
 }
+
+output "address" {
+  value       = aws_db_instance.example.address
+  description = "Connect to the database at this endpoint"
+}
+
+output "port" {
+  value       = aws_db_instance.example.port
+  description = "The port the database is listening on"
+}
